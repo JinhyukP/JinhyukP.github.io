ef5fefdb71ac63057b5ef36e4a7994b727f8897a
Jinhyuk <jhpark@robotics.snu.ac.kr>
2023-04-11 20:02:37 +0900

    update gan_1d.py

--- practice/GAN/gan_1d.py
+++ practice/GAN/gan_1d.py
@@ -6,6 +6,8 @@ import math
 import os
 import argparse
 
+import torch.nn as nn
+
 
 
 parser = argparse.ArgumentParser()
@@ -21,6 +23,8 @@ parser.add_argument("--channels", type=int, default=1, help="number of image cha
 parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
 opt = parser.parse_args()
 print(opt)
+
+cuda = True if torch.cuda.is_available() else False
 
 class Sines():
     def __init__(self):
@@ -43,21 +47,140 @@ class Sines():
         dataset = torch.from_numpy(dataset)
         return dataset
     
+# Configure data loader
+os.makedirs("data/sines", exist_ok=True)
+# dataloader = torch.utils.data.DataLoader(
+    
+#     # TODO
+    
+#     batch_size=opt.batch_size,
+#     shuffle=True,
+# )
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        # TODO
+        # def block(in_feat, out_feat, normalize=True):
+        #     layers = [nn.Linear(in_feat, out_feat)]
+        #     if normalize:
+        #         layers.append(nn.BatchNorm1d(out_feat, 0.8))
+        #     layers.append(nn.LeakyReLU(0.2, inplace=True))
+        #     return layers
+
+        # TODO
+        # self.model = nn.Sequential(
+            # *block(opt.latent_dim, 128, normalize=False),
+            # *block(128, 256),
+            # *block(256, 512),
+            # *block(512, 1024),
+            # nn.Linear(1024, int(np.prod(img_shape))),
+            # nn.Tanh()
+        # )
+
+    # TODO
+    # def forward(self, z):
+        # img = self.model(z)
+        # img = img.view(img.size(0), *img_shape)
+        # return img
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        # TODO
+        # self.model = nn.Sequential(
+        #     nn.Linear(int(np.prod(img_shape)), 512),
+        #     nn.LeakyReLU(0.2, inplace=True),
+        #     nn.Linear(512, 256),
+        #     nn.LeakyReLU(0.2, inplace=True),
+        #     nn.Linear(256, 1),
+        #     nn.Sigmoid(),
+        # )
+
+    # TODO
+    # def forward(self, img):
+    #     img_flat = img.view(img.size(0), -1)
+    #     validity = self.model(img_flat)
+    #     return validity
+
+# Loss function
+adversarial_loss = torch.nn.BCELoss()
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+    
+# Optimizers
+# optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+# optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+
 
 if __name__=="__main__":
     print("===== main running =====")
-    for epoch in range(opt.n_epochs):
-        for i, (imgs, _) in enumerate(dataloader):    
-            pass
-#         # Adversarial ground truths            
-            
-#         # Configure input            
-            
-#         # -----------------
-#         #  Train Generator
-#         # -----------------
+        
+        
+# ----------
+#  Training
+# ----------
+        
+    # for epoch in range(opt.n_epochs):
+    #     for i, (imgs, _) in enumerate(dataloader):
 
+    #         # Adversarial ground truths
+    #         valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+    #         fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
 
-#         # ---------------------
-#         #  Train Discriminator
-#         # ---------------------
+    #         # Configure input
+    #         real_imgs = Variable(imgs.type(Tensor))
+
+    #         # -----------------
+    #         #  Train Generator
+    #         # -----------------
+
+    #         optimizer_G.zero_grad()
+
+    #         # Sample noise as generator input
+    #         z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+
+    #         # Generate a batch of images
+    #         gen_imgs = generator(z)
+
+    #         # Loss measures generator's ability to fool the discriminator
+    #         g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+    #         g_loss.backward()
+    #         optimizer_G.step()
+
+    #         # ---------------------
+    #         #  Train Discriminator
+    #         # ---------------------
+
+    #         optimizer_D.zero_grad()
+
+    #         # Measure discriminator's ability to classify real from generated samples
+    #         real_loss = adversarial_loss(discriminator(real_imgs), valid)
+    #         fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+    #         d_loss = (real_loss + fake_loss) / 2
+
+    #         d_loss.backward()
+    #         optimizer_D.step()
+
+    #         print(
+    #             "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+    #             % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+    #         )
+
+    #         batches_done = epoch * len(dataloader) + i
+    #         if batches_done % opt.sample_interval == 0:
+    #             save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+
